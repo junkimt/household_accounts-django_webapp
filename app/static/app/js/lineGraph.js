add4d8ca146ea07a6a15065b2ab2a1ffe4ed2cda
mizutani <mizutani@cv.info.gifu-u.ac.jp>
2019-06-23 08:36:01 +0900

    [update]

--- app/static/app/js/lineGraph.js
+++ app/static/app/js/lineGraph.js
@@ -1,10 +1,19 @@
 function showLineGraph (line_graph, id) {
 
+  console.log('in showLineGraph')
+
   var dataset = line_graph
 
   var width = parseInt(window.innerWidth * 0.9); // グラフの幅
-  var height = parseInt(width * 0.4); // グラフの高さ
-  var margin = { "top": 30, "bottom": 60, "right": 30, "left": 60 };
+  //var height = parseInt(width * 0.4); // グラフの高さ
+  var height = 350
+  var margin = { "top": 30, "bottom": 60, "right": 10, "left": 130 };
+
+  var maxYenDefault = 50000
+  var maxYenDataset = d3.max(dataset, function(d) { return d[1]; })
+  if (maxYenDefault > maxYenDataset){
+    maxYenDataset = maxYenDefault;
+  }
 
   // 2. SVG領域の設定
   var id_str = "#" + id;
@@ -16,12 +25,14 @@ function showLineGraph (line_graph, id) {
     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
     .range([margin.left, width - margin.right]);
   */
+
+  console.log(maxYenDataset)
   var xScale = d3.scaleLinear()
     .domain([0, 31])
     .range([margin.left, width - margin.right]);
 
   var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+    .domain([0, maxYenDataset])
     .range([height - margin.bottom, margin.top]);
 
   // 4. 軸の表示
@@ -39,7 +50,7 @@ function showLineGraph (line_graph, id) {
     .attr("text-anchor", "middle")
     .attr("font-size", "10pt")
     .attr("font-weight", "bold")
-    .text("Date");
+    .text("日付");
 
 
   svg.append("g")
@@ -49,11 +60,11 @@ function showLineGraph (line_graph, id) {
     .attr("fill", "black")
     .attr("text-anchor", "middle")
     .attr("x", -(height - margin.top - margin.bottom) / 2 - margin.top)
-    .attr("y", -35)
+    .attr("y", -60)
     .attr("transform", "rotate(-90)")
     .attr("font-weight", "bold")
     .attr("font-size", "10pt")
-    .text("yen");
+    .text("１日ごとの使用金額");
 
   // 5. ラインの表示
   svg.append("path")
