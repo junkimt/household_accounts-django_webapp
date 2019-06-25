d4309b4a5390a270188046d2539fc5dcdb399d23
mizutani <mizutani@cv.info.gifu-u.ac.jp>
2019-06-25 11:01:37 +0900

    [fix] static root

--- config/settings.py
+++ config/settings.py
@@ -215,7 +215,7 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 # https://qiita.com/saira/items/a1c565c4a2eace268a07
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app', 'staticfiles')
 DEBUG = False
 try:
     from .local_settings import *
